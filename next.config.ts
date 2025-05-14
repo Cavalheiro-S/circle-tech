116da2aebcedb14a69224bcc41c013e378e1685b
Lucas Cavalheiro <luccribeiro53@gmail.com>
2025-05-14 16:19:29 -0300

    feat: alterando o layout dos cards de projeto

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   distDir: "build",
   output: "export",
   images:{
     unoptimized: true,
   },
-  trailingSlash: true,    // coloca index.html em cada rota
+  trailingSlash: true,
 };
 
 export default nextConfig;
